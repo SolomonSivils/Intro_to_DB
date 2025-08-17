1c9dec93b8eff55c75ccd7fd6f863d7522650a5a
Eric Macharia <iriqmash21@gmail.com>
2025-08-18 00:03:47 +0300

    Task 6

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,7 @@
 -- This script inserts multiple rows into the 'customer' table of the 'alx_book_store' database.
 -- The database name will be provided as an argument when the script is run.
 
-INSERT INTO customer (customer_id, customer_name, email, address)
+INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES
     (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
     (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
