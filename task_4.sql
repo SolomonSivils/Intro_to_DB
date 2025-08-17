e4b91ea87eb77bc842c7416443b4616a3f9f1ba9
Eric Macharia <iriqmash21@gmail.com>
2025-08-17 23:33:49 +0300

    Task 4

--- task_4.sql
+++ task_4.sql
@@ -1,15 +1,13 @@
-USE alx_book_store;
-
-SELECT
+SELECT 
     COLUMN_NAME,
     COLUMN_TYPE,
     IS_NULLABLE,
     COLUMN_KEY,
     COLUMN_DEFAULT,
     EXTRA
-FROM
+FROM 
     INFORMATION_SCHEMA.COLUMNS
-WHERE
-    TABLE_SCHEMA = 'alx_book_store'
-AND
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+AND 
     TABLE_NAME = 'books';
