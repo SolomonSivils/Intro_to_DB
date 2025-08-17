f1a7e4e23b8b4dfb9273a1e61f578855657f812b
Eric Macharia <iriqmash21@gmail.com>
2025-08-17 23:15:57 +0300

    Task 1

--- MySQLServer.py
+++ MySQLServer.py
@@ -7,6 +7,8 @@ DB_PASSWORD = "mycountryKenya254"
 DATABASE_NAME = "alx_book_store"
 
 def create_database():
+    cnx = None
+    cursor = None
 
     try:
         cnx = mysql.connector.connect(
@@ -15,7 +17,9 @@ def create_database():
             password=DB_PASSWORD
         )
         cursor = cnx.cursor()
+
         create_db_query = f"CREATE DATABASE IF NOT EXISTS {DATABASE_NAME}"
+        
         cursor.execute(create_db_query)
 
         print(f"Database '{DATABASE_NAME}' created successfully!")
@@ -23,15 +27,13 @@ def create_database():
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
             print("Error: Access denied. Check your username and password.")
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Error: Database does not exist.")
         else:
             print(f"Error: {err}")
 
     finally:
-        if 'cursor' in locals() and cursor is not None:
+        if cursor is not None:
             cursor.close()
-        if 'cnx' in locals() and cnx is not None and cnx.is_connected():
+        if cnx is not None and cnx.is_connected():
             cnx.close()
             print("MySQL connection is closed.")
 
