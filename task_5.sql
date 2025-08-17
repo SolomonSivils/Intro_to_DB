aae86ec61a1af5815b4f7aa1e602a6891a4caf47
Eric Macharia <iriqmash21@gmail.com>
2025-08-17 23:48:46 +0300

    Task 5

--- task_5.sql
+++ task_5.sql
@@ -1,8 +1,5 @@
-USE alx_book_store;
+-- This script inserts a single row into the 'customer' table of the 'alx_book_store' database.
+-- The database name will be provided as an argument when the script is run.
 
-INSERT INTO Customers (
-    customer_id = 1, 
-    customer_name = 'Cole Baidoo', 
-    email = 'cbaidoo@sandtech.com', 
-    address = '123 Happiness Ave.'
-    );
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
